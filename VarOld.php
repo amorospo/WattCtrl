f3f4dd0c320b9a802a723b445ecd5c17e9341313
amorospo <amorospo@yahoo.it>
2017-02-17 22:58:51 +0100

    Update VarOld.php

--- VarOld.php
+++ VarOld.php
@@ -1,12 +1,12 @@
 <?php
-$site = "opera";
-$Watt = “/dev/shm/metern2.txt";
-$met_W = “2”;
+$site = "Mio sito";
+$Watt = "/dev/shm/metern2.txt";
+$met_W = "2";
 $smtp_S = "smtp.gmail.com";
 $smtp_P = 587;
-$from_addr = “email@gmail.com";
-$pwd = “password”;
-$to_addrs = “emailto@email.email”;
+$from_addr = "email@gmail.com";
+$pwd = "password";
+$to_addrs = "emailto@email.email";
 $Sw_Off = 0;
 $LowW = 100;
 $HiW = 3000;
